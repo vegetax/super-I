9aa40005c3eafaf1dfb757c5d3b66692e013f5b6
JesseB <xmzedu@gmail.com>
2022-04-14 15:42:53 +0800

    no

--- test/Kryptobirds.js
+++ test/Kryptobirds.js
@@ -1,5 +1,5 @@
-const { assert } = require("chai");
-require("chai").use(require("chai-as-promised")).should();
+// const { assert } = require("chai");
+// require("chai").use(require("chai-as-promised")).should();
 
 const Kryptobirdz = artifacts.require("Kryptobirdz");
 
@@ -13,5 +13,11 @@ contract("Kryptobirdz", (accounts) => {
   it("contract has a name", async () => {
     const name = await kryptobirdz.name();
     assert.equal(name, "Kryptobird");
+  });
+  it("creat a new token", async () => {
+    const hx = await kryptobirdz.safeMint(accounts[1], "birdz1");
+    const totalsupply = await kryptobirdz.totalSupply();
+    assert.equal(totalsupply, 1);
+    assert.equal(await kryptobirdz.ownerOf(0), accounts[1]);
   });
 });
